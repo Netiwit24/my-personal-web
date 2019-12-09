048ce859a710a46f45b517dd083575e33af49ef4
neativit24 <neativit04@gmail.com>
2019-12-09 15:32:14 +0700

    Updates

---
+++ precache-manifest.53f9bdbfc1304793cc4fc0f470eccb85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ac4739966b73a856eee47d51c5ba97",
+    "url": "/my-personal-web/index.html"
+  },
+  {
+    "revision": "a763975ab506bc133227",
+    "url": "/my-personal-web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0b0c34bcd869df6fb95f",
+    "url": "/my-personal-web/static/js/2.35fc71d4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-personal-web/static/js/2.35fc71d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a763975ab506bc133227",
+    "url": "/my-personal-web/static/js/main.da6b4f62.chunk.js"
+  },
+  {
+    "revision": "81a9d0200791f6efe1f2",
+    "url": "/my-personal-web/static/js/runtime-main.cb2b65e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-personal-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
